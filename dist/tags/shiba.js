07421af51e7544e5398226e4fdb7e2dcd8e7e6b3
幾野直人 <moon_in_france@yahoo.co.jp>
2016-12-22 19:56:48 +0900

    fix bug

--- dist/tags/shiba.js
+++ dist/tags/shiba.js
@@ -1,16 +1,17 @@
 riot.tag2('shiba', '<div class="week" each="{week, i in weeks}"> <div class="{day: true, shiba: isShiba[i][j]}" each="{day, j in week}"></div> </div>', 'shiba,[data-is="shiba"]{ display: flex; flex-direction: row; } shiba .week,[data-is="shiba"] .week{ display: flex; flex-direction: column; } shiba .day,[data-is="shiba"] .day{ background: #eee; display: inline-block; height: 10px; width: 10px; margin: 1px; } shiba .shiba,[data-is="shiba"] .shiba{ background: #44a340; }', '', function(opts) {
-this.weeks = createYear(2016);
-
-this.isShiba = shibaTable(2016, this.weeks);
-var upper = Util.upperBoundCommits(Util.commits, new Date(2016, 11, 31));
-var lower = Util.lowerBoundCommits(Util.commits, new Date(2016, 0, 1));
-console.log(Util.binarySearch(
-  Util.commits.slice(lower, upper),
-  new Date(2016, 11, 22)
-));
+  var year = new Date().getFullYear();
+this.weeks = createYear(year);
+this.isShiba = shibaTable(year, this.weeks);
+var self = this;
+Util.commits.on('change', function() {
+  self.isShiba = shibaTable(year, self.weeks);
+  self.update();
+});
 function shibaTable(year, calendar) {
   var month = -1;
   var res = [];
+  var upper = Util.upperBoundCommits(Util.commits, new Date(year, 11, 31));
+  var lower = Util.lowerBoundCommits(Util.commits, new Date(year, 0, 1));
   for (var i = 0, _i = calendar.length; i < _i; i++) {
     res.push([]);
     for (var j = 0, _j = calendar[i].length; j < _j; j++) {
